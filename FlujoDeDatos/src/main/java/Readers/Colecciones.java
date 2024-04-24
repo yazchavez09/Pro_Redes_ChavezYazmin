7daef32d8ec455683fddfc94062de717bff803d2
Yazmin Chavez <yazmin.chavez.et32@gmail.com>
2024-04-24 12:46:37 -0300

    colecciones parte2

--- FlujoDeDatos/src/main/java/Readers/Colecciones.java
+++ FlujoDeDatos/src/main/java/Readers/Colecciones.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Colecciones {
 	//vector
@@ -17,25 +20,49 @@ public class Colecciones {
 	
 	//ArayList
 	ArrayList<String> frutas = new ArrayList<>();
-	ArrayList verduras = new ArrayList<>();
+	ArrayList verduras = new ArrayList<>(); 
+	
+	LinkedList<String> componentesPc1 = new LinkedList<>(); 
+	//LinkedList: listas (funciona igual q el array).Pero es eficiente a la hora de agg inf nueva.
+	List precios1 = new LinkedList<>(); //
 	
 	LinkedList<String> componentesPc = new LinkedList<>();
 	List precios = new LinkedList<>();
 	
-	LinkedList<String> componentesPc = new LinkedList<>();
-	List precios = new LinkedList<>();
-	LinkedHashMap<String, String> listaEspaÃ±olIngles = new LinkedHashMap<>();
+	HashMap<String, String> listaEspañolIngles = new LinkedHashMap<>();
 	Map<Integer, Integer> valores = new LinkedHashMap<>();
 
-	LinkedHashSet<Integer> listaInglesEspaÃ±ol = new LinkedHashSet<>();
+	LinkedHashSet<Integer> listaInglesEspañol = new LinkedHashSet<>();
 	Set<Integer> valores2 = new LinkedHashSet<>();
-
-	HashMap<String, String> espaÃ±olRAE = new HashMap<>();
-	Map<String, Integer> espaÃ±olIngles = new HashMap<>();
-
+	
+	//Diccionario: clave, valor.
+	HashMap<String, String> españolRAE = new HashMap<>();
+	Map<String, Integer> españolIngles = new HashMap<>();
+	
+	
 	HashSet<String> claves = new HashSet<>();
 	Set<Integer> DNI = new HashSet<>();
-
+	//se utiliza como un array pero todos os datos de un set son unicos.
+	
+	public void Array()
+	{
+		frutas.add("");
+		verduras.add(5); //objec puedo mandar lo que yo quiera.
+		verduras.add("s");
+		
+		frutas.clear(); //vacia por completo
+		frutas.remove(5);// lo que esta en la posicion 5
+		frutas.remove("manzana"); //busca y borra ese dato particular.
+		
+		verduras.contains("tomate"); //existe o no (boolean)
+		verduras.containsAll(new ArrayList<>()); 
+		
+		frutas.get(5); //devuelve lo que esta en la posicion 5.
+		frutas.indexOf("banana"); // busca banana y me devuelve la posicion.
+		
+		verduras.isEmpty(); // tiene algo o no tiene nada
+		verduras.size(); // cant de elememtos que tiene la lista
+	}
 	
 	
 }
