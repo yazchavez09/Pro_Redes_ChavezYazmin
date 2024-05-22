b0db191eb8ba944a243157b2f1c058ba232762d9
Yazmin Chavez <yazmin.chavez.et32@gmail.com>
2024-05-22 11:13:16 -0300

    clase Fichero

--- files/src/main/java/Archivos/Ficheros.java
+++ files/src/main/java/Archivos/Ficheros.java
@@ -1,5 +1,40 @@
 package Archivos;
 
-public class Ficheros {
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.io.FileOutputStream;
+	import java.io.PrintStream;
 
+	public class Ficheros {
+		
+		private File archivo;
+		private PrintStream ps;
+		
+		public Ficheros()
+		{
+			String ruta = "";  //   " c:\\User\\ "
+			String nombre = "mario";
+			String extension = ".txt";
+			
+			archivo =  new File( ruta.concat(nombre.concat(extension))  );
+		}
+		
+		public void createFilePrintStream(File a)
+		{
+			FileOutputStream fos = null;
+			
+			try {
+				fos = new FileOutputStream(a);
+				ps = new PrintStream(fos , true); //mode append true = no sobreescribe
+				
+				ps.println("holaa mundo");
+				ps.println("Chauu mundo");
+				
+				ps.flush();
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+		}
 }
