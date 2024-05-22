80bc89397d235b6d48e3b70fa3588455dba184f9
Yazmin Chavez <yazmin.chavez.et32@gmail.com>
2024-05-22 12:39:46 -0300

    22/05

--- files/src/main/java/Archivos/Ficheros.java
+++ files/src/main/java/Archivos/Ficheros.java
@@ -1,40 +1,115 @@
 package Archivos;
 
-	import java.io.File;
-	import java.io.FileNotFoundException;
-	import java.io.FileOutputStream;
-	import java.io.PrintStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
-	public class Ficheros {
+public class Ficheros {
+
+	private File archivo;
+	private PrintStream ps;
+
+	public Ficheros() {
+		String ruta = ""; // " c:\\User\\ "
+		String nombre = "mario";
+		String extension = ".txt";
 		
-		private File archivo;
-		private PrintStream ps;
+	//System.setErr(new PrintStre (COMPLETAR)
 		
-		public Ficheros()
-		{
-			String ruta = "";  //   " c:\\User\\ "
-			String nombre = "mario";
-			String extension = ".txt";
-			
-			archivo =  new File( ruta.concat(nombre.concat(extension))  );
+		archivo = new File(ruta.concat(nombre.concat(extension)));
+	}
+
+	public File getArchivo() {
+		return this.archivo;
+	}
+
+	public void createFilePrintStream(File a) {
+
+		FileOutputStream fos = null;
+
+		try {
+			fos = new FileOutputStream(a);
+			ps = new PrintStream(fos, true); // mode append true = no sobreescribe
+
+			ps.println("holaa mundo");
+			ps.println("Chauu mundo");
+
+			ps.flush();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (fos != null)
+				try {
+					fos.close();
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
+
 		}
-		
-		public void createFilePrintStream(File a)
-		{
-			FileOutputStream fos = null;
-			
+
+	}
+
+	public void createFilePrinter(File a) {
+
+		FileWriter fw = null;
+		PrintWriter pw = null;
+
+		try {
+
+			if (a.exists()) {
+				a.createNewFile();
+			}
+
+			fw = new FileWriter(a, true);
+			pw = new PrintWriter(fw);
+
+			pw.println("Otro MUNDO");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
 			try {
-				fos = new FileOutputStream(a);
-				ps = new PrintStream(fos , true); //mode append true = no sobreescribe
-				
-				ps.println("holaa mundo");
-				ps.println("Chauu mundo");
-				
-				ps.flush();
-				
-			} catch (FileNotFoundException e) {
+				if (pw != null)
+					pw.close();
+
+				if (fw != null)
+					fw.close();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
 		}
-}
+	}
+
+	public void createFileBuffer(File a) {
+		
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter (a, true);
+		    bw = new BufferedWriter (fw);
+		    
+		    bw.write("Explotemos al MUNDO");
+		    bw.newLine();
+		    bw.write("Mejor creemos un mundo nuevo");
+		    bw.flush();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}//finally {
+			//try {
+				//if(pw!=null)
+			//}
+			
+		//}
+		
+	}
+	}
+
--- files/src/main/java/Archivos/main.java
+++ files/src/main/java/Archivos/main.java
@@ -4,7 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Ficheros archivo = new Ficheros();
+		//archivo.createFilePrintStream(archivo.getArchivo());
+		archivo.createFilePrinter(archivo.getArchivo());
+		
 	}
 
 }
